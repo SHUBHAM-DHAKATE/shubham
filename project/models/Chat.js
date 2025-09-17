f2aef46c2f432343598deced13ad9076fe652e4b
SHUBHAM-DHAKATE <164625627+SHUBHAM-DHAKATE@users.noreply.github.com>
2025-09-17 19:54:19 +0530

    Delete project directory

--- project/models/Chat.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const chatSchema = new mongoose.Schema({
-  chat_type: { type: String, enum: ["1:1", "group"], required: true },
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-}, { timestamps: { createdAt: "created_at" } });
-
-module.exports = mongoose.model("Chat", chatSchema);
